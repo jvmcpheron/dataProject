6e6309b0740b45fd2a1d160dad109f1e2e7ea3c4
Jane McPheron <jvmcpheron@dyn-10-140-246-187.d.umn.edu>
2023-02-10 13:33:15 -0600

    added a function that allows user to view data from csv file in terminal

--- dataProject.cpp
+++ dataProject.cpp
@@ -16,26 +16,42 @@ void createData()
 	// opens an existing csv file or creates a new file.
 	fout.open("data.csv", ios::out | ios::app);
 
-	cout << "Enter their name and their favorite food:"
-		<< " name food" << endl;
+	cout << "Enter their id number, name and favorite food:"
+		<< " number name food" << endl;
 
 	int i;
+    int number;
     string food;
 	string name;
 
 	// Read the input
 	for (i = 0; i < 3; i++) {
 
-		cin >> name
+		cin >> number
+            >> name
 			>> food;
 
 		// Insert the data to file
-		fout << name << ", "
+		fout << number << ", "
+            << name << ", "
 			<< food << "\n";
 	}
 }
 
+//function to read specific data
+void readData(){
 
+    ifstream myFile;
+    myFile.open("data.csv");
+
+    while (myFile.good()){
+        string line;
+        getline(myFile, line, ',');
+        cout << line << endl;
+    }
+
+
+}
 
 int main (){
 
@@ -44,17 +60,21 @@ string tempAnswer = "ahhhh";
 
 do {
 
-cout << "What would you like to do with the data? (create, done)" << endl;
+cout << "What would you like to do with the data? (create, done, read)" << endl;
 cin >> tempAnswer;
-if (tempAnswer == "done" || tempAnswer == "create"){
+if (tempAnswer == "done" || tempAnswer == "create" || tempAnswer == "read"){
     keepGoing = tempAnswer;
     if (keepGoing == "create"){
         createData();
+    }else if (keepGoing == "read"){
+        readData();
     }
 } else{
     cout << "Try again.";
 }
 
 } while (keepGoing != "done");
+
+
 
 }
